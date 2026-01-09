c5af8592f5c279af8fb085f6cd2190fa2beac78c
Yura Chepurnykh <yura.chepurnykh@gmail.com>
2026-01-09 10:15:43 -0500

    refactor(models/enums): remove enums

---
+++ models/include/traits.hpp
@@ -0,0 +1,22 @@
+#ifndef TRAITS_HPP
+#define TRAITS_HPP
+
+#include <type_traits>
+
+#include "tags.hpp"
+#include "enums.hpp"
+
+template<typename LayerTag>
+struct Traits;
+
+template<typename Type, typename DopantMaterial, size_t N>
+struct Traits<Substrate<Type, DopantMaterial, N>>
+{
+    using DopantType = typename CarrierMap<DopantMaterial>::type;
+    using CarrierType = 
+};
+
+
+
+
+#endif // TRAITS_HPP
