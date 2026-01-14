2b163cd8d46daf06b9a4768403be59c259c73236
Yura Chepurnykh <yura.chepurnykh@gmail.com>
2026-01-14 10:24:24 -0500

    feat(cli/view): add input view for CLI commands

---
+++ cli/view/include/view.hpp
@@ -0,0 +1,13 @@
+#ifndef VIEW_HPP
+#define VIEW_HPP
+
+#include <iostream>
+#include <string>
+
+class View
+{
+public:
+    std::string operator()();
+};
+
+#endif // VIEW_HPP
---
+++ cli_view/src/view.cpp
@@ -0,0 +1,8 @@
+#include "view.hpp"
+
+std::string View::operator()()
+{
+    std::cout << "> ";
+    std::string input;
+    std::getline(std::cin, input);
+}
