7b270b882f698662c1fb864419fe6dffd895de12
Yura Chepurnykh <yura.chepurnykh@gmail.com>
2026-02-14 06:19:09 -0500

    feat(editor/presenters): implement Builder pattern for rectangle and polygon creation

---
+++ editor/presenters/builder.cpp
@@ -0,0 +1,59 @@
+#include "builder.hpp"
+
+IShapeBuilder& RectBuilder::onPress(const Point& p)
+{
+    m_rect.setTopLeft(p);
+    return *this;
+}
+
+IShapeBuilder& RectBuilder::onMove(const Point& p)
+{
+    m_rect.setBottomRight(p);
+    return *this;
+}
+
+IShapeBuilder& RectBuilder::onRelease(const Point& p)
+{
+    onMove(p);
+    return *this;
+}
+
+IShapeBuilder& RectBuilder::onDouble(const Point&) { }
+
+std::unique_ptr<IShape> RectBuilder::build()
+{
+    return std::make_unique<Rect>(m_rect);
+}
+
+IShapeBuilder& PolygonBuilder::onPress(const Point& p)
+{
+    if (m_polygon.m_points.empty())
+    {
+        m_polygon.m_points.push_back(p);
+        m_polygon.m_points.push_back(p);
+    }
+    else
+    {
+        m_polygon.m_points.back() = p;
+        m_polygon.m_points.push_back(p);
+    }
+
+    return *this;
+}
+
+IShapeBuilder& PolygonBuilder::onMove(const Point& p)
+{
+    m_polygon.m_points.back() = p;
+    return *this;
+}
+
+IShapeBuilder& PolygonBuilder::onDouble(const Point& p)
+{
+    onPress(p);
+    return *this;
+}
+
+std::unique_ptr<IShape> PolygonBuilder::build()
+{
+    return std::make_unique<Polygon>(m_polygon);
+}
---
+++ editor/presenters/builder.hpp
@@ -0,0 +1,47 @@
+#ifndef BUILDER_HPP
+#define BUILDER_HPP
+
+#include <type_traits>
+#include <QPointF>
+#include <QRectF>
+#include <QPolygonF>
+#include <QPainterPath>
+#include "geometry.hpp"
+
+struct IShapeBuilder
+{
+    virtual ~IShapeBuilder() = default;
+    virtual IShapeBuilder& onPress(const Point& p) = 0;
+    virtual IShapeBuilder& onMove(const Point& p) = 0;
+    virtual IShapeBuilder& onRelease(const Point& p) = 0;
+    virtual IShapeBuilder& onDouble(const Point& p) = 0;
+    virtual std::unique_ptr<IShape> build() = 0;
+};
+
+class RectBuilder final : public IShapeBuilder
+{
+public:
+    RectBuilder() = default;
+    IShapeBuilder& onPress(const Point& p) override;
+    IShapeBuilder& onMove(const Point& p) override;
+    IShapeBuilder& onRelease(const Point& p) override;
+    IShapeBuilder& onDouble(const Point&) override;
+    std::unique_ptr<IShape> build() override;
+
+private:
+    Rect m_rect;
+};
+
+class PolygonBuilder final : public IShapeBuilder
+{
+public:
+    IShapeBuilder& onPress(const Point& p) override;
+    IShapeBuilder& onMove(const Point& p) override;
+    IShapeBuilder& onDouble(const Point& p) override;
+    std::unique_ptr<IShape> build() override;
+
+private:
+    Polygon m_polygon;
+};
+
+#endif // BUILDER_HPP
